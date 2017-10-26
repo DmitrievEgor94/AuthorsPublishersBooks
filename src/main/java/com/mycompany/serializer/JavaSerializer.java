360739a3545177db258f2f6d55e9b5e0f2f7ee5b
Dmitriev Egor <DmitrievEgor94@yandex.ru>
2017-10-26 20:26:24 +0400

    Create entities for original models, added hex and textFormat serialization classes and class for testing them.

---
+++ src/main/java/com/mycompany/serializer/JavaSerializer.java
@@ -0,0 +1,45 @@
+package com.mycompany.serializer;
+
+import java.io.*;
+import java.util.List;
+
+public class JavaSerializer implements Serializer {
+
+    @Override
+    public void serializeObject(Object ob, String fileWithObjects) throws IOException {
+        ObjectOutputStream out =
+                new ObjectOutputStream(
+                        new FileOutputStream(
+                                new File(fileWithObjects)));
+
+        out.writeObject(ob);
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    public Object deserializeObject(String fileWithObjects, Class obClass) throws IOException, ClassNotFoundException {
+        ObjectInputStream in =
+                new ObjectInputStream(
+                        new FileInputStream(
+                                new File(fileWithObjects)));
+
+        Object ob = in.readObject();
+
+        if (ob instanceof List) {
+            List collection = (List) ob;
+
+            //Using loop is the only way to check type of Object in list
+            for (Object o : collection) {
+                if (o.getClass() == obClass) {
+                    break;
+                }
+                else {
+                    throw new ClassNotFoundException();
+                }
+            }
+        }
+
+        return ob;
+    }
+}
